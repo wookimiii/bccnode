682247513cc973010f28f2ea1ac45d7cbf41be50
andrew kim <akim@attinteractive.com>
2012-12-02 10:49:41 -0800

    its a good point to commit, wrote a ton of stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,52 @@
 var app = angular.module('app', ['catechisms']);
+app.config(function($routeProvider, $locationProvider) {
+  // $locationProvider.html5Mode(true);
+  $routeProvider.
+    when('/new', {controller:NewCtrl, template:$('#new').html()}).
+    otherwise({controller:ListCtrl, template:$('#list').html()});
+  });
+
+function MainCtrl($scope, Catechism, $location){
+  $scope.fetchData = function(){
+    $scope.catechisms = Catechism.query();
+  }
+  $scope.fetchData();
+
+  $scope.goHome = function(){
+    $location.path("");
+  }
+}
 
 function ListCtrl($scope, Catechism){
-  $scope.catechisms = Catechism.query();
   
   $scope.debug = function(){
     console.log($scope.catechisms);
   }
 
+}
+
+function NewCtrl($scope, Catechism){
+  $scope.new = {
+    number: 3,
+    question: "What do the Scriptures principally teach?",
+    answer: "The Scriptures principally teach, what man is to believe concerning God, and what duty God requires of man."
+  }
+
+  $scope.verses = "Genesis 1:1;John 5:39";
+
+  $scope.save = function(){
+    console.log($scope.new.verses);
+    var catechism = new Catechism($scope.new);
+    console.log(catechism);
+    catechism.$save(function(){
+      $scope.fetchData();
+      $scope.goHome();
+    });
+  }
+
+  $scope.parseVerses = function(){
+    $scope.new.verses = $scope.verses.split(";");
+  }
+
+  $scope.$watch('verses', $scope.parseVerses);
 }
