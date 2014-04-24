3a17a319acb61eae9f8ae1c78085821db9208c03
Andrew Kim <akim@yp.com>
2014-04-23 19:03:39 -0700

    Starting over

--- app.js
+++ app.js
@@ -1,57 +1,12 @@
-var express = require('express');
-var path = require('path');
-var stylus = require('stylus');
+var http = require('http')
 
-/**
- * Handlers
-**/
-var catechisms = require('./handlers/catechisms.js');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('hello world');
+});
 
-/**
- * Initialize the express app
-**/
-var app = express();
-app.use(express.bodyParser());
-
-/**
- * Jade Templates
-**/
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-// app.set('view cache', true);
-app.set('etag', false);
-
-/**
- * Use the public directory for assests
-**/ 
-app.use(express.static(__dirname + '/assets'))
-
-/**
- * Application port
- * Heroku uses process.env.PORT
-**/
 var port = process.env.PORT || 5000;
+server.listen(port);
+console.log("Listening on port: " + port);
 
-
-/**
- * Start the server
-**/ 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-/**
- * Routes
-**/
-app.get('/', function(req, res) {
-    console.log("Welcome to BCC");
-    res.render('index');
-});
-
-// +++ CATECHISMS 
-app.get('/shorter-catechisms', catechisms.index);
-app.get('/shorter-catechisms/new', catechisms.newForm);
-app.post("/api/catechisms", catechisms.create);
-app.get('/api/catechisms', catechisms.all);
-
-module.exports = app
+module.exports = server;
