446cb9f199faa41d893e6f0591810d3771deefaf
Andrew Kim <akim@yp.com>
2014-05-12 20:51:34 -0700

    Serving assets

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var http = require('http')
+var fs = require('fs');
+var st = require('st');
 
+var mount = st({ path: __dirname + '/public', url: '/assets' })
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('hello world');
+    // static assets
+    if (mount(req, res)) return;
+
+    console.log(req.url);
+    if (req.url === "/") {
+        // index.html
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile("./views/index.html", function(err, data) {
+            if (err) throw err;
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404);
+        res.end(req.url + "Not Found");
+    }
 });
 
 var port = process.env.PORT || 5000;
